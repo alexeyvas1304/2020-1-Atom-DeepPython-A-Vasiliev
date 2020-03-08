c7f9ebb3c283114338c4895a926bb9797e78e0f2
Alexey <nikson1708@yandex.ru>
2020-03-09 01:16:14 +0300

    добавил .gitignore, requirements.txt и задачу про список

---
+++ homework2/superlist.py
@@ -0,0 +1,57 @@
+from itertools import zip_longest
+from functools import wraps
+
+
+def validate_second_operand(func):
+    @wraps(func)
+    def wrapper(*args):
+        if not isinstance(args[1], SuperList):
+            raise Exception('Второй операнд не суперсписок')
+        return func(*args)
+    return wrapper
+
+
+class SuperList(list):
+
+    def __init__(self, lst):
+        if not isinstance(lst, list):
+            raise Exception('Наследование не от списка')
+        for element in lst:
+            if not isinstance(element, int):
+                raise Exception('В переданном конструктору списке не целые числа')
+        super().__init__(lst)
+
+    @validate_second_operand
+    def __add__(self, other):
+        return SuperList(list(map(lambda x: x[0] + x[1], zip_longest(self, other, fillvalue=0))))
+
+    @validate_second_operand
+    def __sub__(self, other):
+        return SuperList(list(map(lambda x: x[0] - x[1], zip_longest(self, other, fillvalue=0))))
+
+    @validate_second_operand
+    def __le__(self, other):
+        return sum(self) <= sum(other)
+
+    @validate_second_operand
+    def __lt__(self, other):
+        return sum(self) < sum(other)
+
+    @validate_second_operand
+    def __ge__(self, other):
+        return sum(self) >= sum(other)
+
+    @validate_second_operand
+    def __gt__(self, other):
+        return sum(self) > sum(other)
+
+    @validate_second_operand
+    def __eq__(self, other):
+        return sum(self) == sum(other)
+
+    @validate_second_operand
+    def __ne__(self, other):
+        return sum(self) != sum(other)
+
+
+print(SuperList([1, 2]) + SuperList([3, 4]) + SuperList([5, 6, 7]) == SuperList([14, 14]))
