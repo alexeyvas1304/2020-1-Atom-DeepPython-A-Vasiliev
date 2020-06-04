f3613b60447d7d7e23aa7740811e17bc8df38856
Alexey <nikson1708@yandex.ru>
2020-06-04 17:46:45 +0300

    правки к последней дз

--- homework10/file_info.py
+++ homework10/file_info.py
@@ -7,18 +7,18 @@ import xlwt
 def walk(path, level):
     for name in os.listdir(path):
         new_path = os.path.join(path, name)
-        abspath = os.path.abspath(new_path)
-        if os.path.isfile(new_path):
-            mark = 'f'
+        if os.path.isfile(new_path) or os.path.isdir(new_path):
+            abspath = os.path.abspath(new_path)
             size = humanize.naturalsize(os.stat(new_path).st_size)
             date_change = datetime.datetime.fromtimestamp(os.stat(new_path).st_mtime).strftime("%Y-%m-%d %H:%M:%S")
-            info.append([name, mark, size, date_change, level, abspath])
-        elif os.path.isdir(new_path):
-            mark = 'd'
-            size = humanize.naturalsize(os.stat(new_path).st_size)
-            date_change = datetime.datetime.fromtimestamp(os.stat(new_path).st_mtime).strftime("%Y-%m-%d %H:%M:%S")
-            info.append([name, mark, size, date_change, level, abspath])
-            walk(new_path, level + 1)
+            info.append([name,
+                         'f' if os.path.isfile(new_path) else 'd',
+                         size,
+                         date_change,
+                         level,
+                         abspath])
+            if os.path.isdir(new_path):
+                walk(new_path, level + 1)
 
 
 def create_table():
